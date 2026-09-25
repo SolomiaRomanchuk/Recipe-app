1667c4be6943b7e5364ada4f9360ae8742eadbcb
Solomia <macbook@MacBook-Air-Macbook.local>
2026-09-25 19:44:15 +0300

    Redirect home page to recipes

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import RedirectView
 from recipes import views
 
 
@@ -10,6 +10,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('profile/', views.ProfileView.as_view(), name='profile'),
+
+    path('', RedirectView.as_view(url='/recipes/', permanent=False)),
 
 
     path(
